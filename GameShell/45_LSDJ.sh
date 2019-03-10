d301db8dbfd96c2a665c50b1f65ea702560fc689
hpcodecraft <thesquidpeople@googlemail.com>
2019-03-10 19:36:50 +0100

    update to latest menu structure

---
+++ GameShell/45_LSDJ.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+retroarch -L /home/cpi/apps/emulators/gambatte_libretro.so "/home/cpi/games/gb/LSDJ 6.5.0.gb"
--- install.sh
+++ install.sh
@@ -3,23 +3,31 @@
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-NEW_APPS_DIR="${HOME}/apps/Menu"
-PATCH_APPS_DIR="${HOME}/launcher/Menu/GameShell"
+APPS_DIR="${HOME}/apps/Menu"
+LAUNCHER_DIR="${HOME}/launcher/Menu/GameShell"
 HERE=`pwd`
-
-
-printf "${GREEN}Installing new menu items...${NC}\n"
-
-cp -R "${HERE}/GameShell/20_Retro Games/*" "${NEW_APPS_DIR}/20_Retro Games/"
-cp -R "${HERE}/GameShell/42_DOOMish" ${NEW_APPS_DIR}
 
 printf "${GREEN}Moving original menu items...${NC}\n"
 
-cd ${PATCH_APPS_DIR}
+cd ${APPS_DIR}
+mv "21_Indie Games" "10_Indie Games"
+# rm -rf "20_Retro Games"
+
+sudo apt-get -y install youtube-dl
+git clone https://github.com/Skayo/clockTube.git 51_clockTube
+
+cd ${LAUNCHER_DIR}
 mv "10_Settings" "97_Settings"
 mv "90_Reload UI.sh" "98_Reload UI.sh"
 mv "97_Music Player" "95_Music Player"
 mv "98_TinyCloud" "96_TinyCloud"
 
-
 rm "32_freeDM.sh"
+
+printf "${GREEN}Installing new menu items...${NC}\n"
+
+cp -R "${HERE}/GameShell/11_My Retro Games" ${APPS_DIR}
+cp -R "${HERE}/GameShell/23_Ports" ${APPS_DIR}
+cp -R "${HERE}/GameShell/42_DOOMish" ${APPS_DIR}
+cp "${HERE}/45_LSDJ.sh" ${APPS_DIR}
+cp "${HERE}/46_nanoloop.sh" ${APPS_DIR}
