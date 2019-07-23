c4e2c1a35912bb5b3ef9df72851c7aec27368e91
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2019-07-23 22:12:49 +0200

    better Clockwork OS 0.4 compatibility

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ NC='\033[0m'
 
 APPS_DIR="${HOME}/apps/Menu"
 LAUNCHER_DIR="${HOME}/launcher/Menu/GameShell"
-HERE=`pwd`
+HERE=$(pwd)
 
 printf "${GREEN}Moving original menu items...${NC}\n"
 
@@ -13,8 +13,8 @@ cd ${APPS_DIR}
 mv "21_Indie Games" "10_Indie Games"
 # rm -rf "20_Retro Games"
 
-sudo apt-get -y install youtube-dl
-git clone https://github.com/Skayo/clockTube.git 51_clockTube
+# sudo apt-get -y install youtube-dl
+# git clone https://github.com/Skayo/clockTube.git 51_clockTube
 
 cd ${LAUNCHER_DIR}
 mv "10_Settings" "97_Settings"
@@ -22,12 +22,12 @@ mv "90_Reload UI.sh" "98_Reload UI.sh"
 mv "97_Music Player" "95_Music Player"
 mv "98_TinyCloud" "96_TinyCloud"
 
-rm "32_freeDM.sh"
+# rm "32_freeDM.sh"
 
 printf "${GREEN}Installing new menu items...${NC}\n"
 
 cp -R "${HERE}/GameShell/11_My Retro Games" ${APPS_DIR}
 cp -R "${HERE}/GameShell/23_Ports" ${APPS_DIR}
 cp -R "${HERE}/GameShell/42_DOOMish" ${APPS_DIR}
-cp "${HERE}/45_LSDJ.sh" ${APPS_DIR}
-cp "${HERE}/46_nanoloop.sh" ${APPS_DIR}
+cp "${HERE}/GameShell/45_LSDJ.sh" ${APPS_DIR}
+cp "${HERE}/GameShell/46_nanoloop.sh" ${APPS_DIR}
